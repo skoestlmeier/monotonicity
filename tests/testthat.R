81a56c3c594ee41ac7c8db3e59746f6cefe62de8
Siegfried Köstlmeier <siegfried.koestlmeier@gmail.com>
2018-05-18 20:41:29 +0200

    Implement testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,5 +2,10 @@
 library(testthat)
 library(monotonicity)
 
-## test package
-test_check('monotonicity')
+## test package separated with filter due to limited Travis-CI build time
+test_check('monotonicity', filter = "monoBonferroni")
+test_check('monotonicity', filter = "monoRelation")
+test_check('monotonicity', filter = "monoSummary")
+test_check('monotonicity', filter = "monoUpDown")
+test_check('monotonicity', filter = "specify_decimal")
+test_check('monotonicity', filter = "wolak")
