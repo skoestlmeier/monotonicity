d5c0deff258a2c8e848380a08f2b26cd9cc28aba
Siegfried Köstlmeier <siegfried.koestlmeier@gmail.com>
2019-08-08 11:18:13 +0200

    Added testthat test-cases for futher development.

--- tests/testthat/test-statBootstrap.R
+++ tests/testthat/test-statBootstrap.R
@@ -6,6 +6,8 @@ test_that('statBootstrap', {
   expect_error(statBootstrap(block_length = 10))
   expect_error(statBootstrap(T = "1", block_length = 10))
   expect_error(statBootstrap(T = -1, block_length = 10))
+  expect_error(statBootstrap(T = , bootstrapRep = "1"))
+  expect_error(statBootstrap(T = 100, bootstrapRep  = -1))
   expect_error(statBootstrap(T = 100))
   expect_error(statBootstrap(T = 100, block_length = 7))
   
